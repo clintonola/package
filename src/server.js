b9a8756e1cceb79b1f7f7cdc006d6f1a0085891c
clintonola <63821901+clintonola@users.noreply.github.com>
2022-10-21 02:12:54 -0400

    improve

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-var express = require('express')
-var app = express()
+let express = require('express')
+let app = express()
 const postgres = require('postgres')
 
 const sql = postgres('postgresql://localhost:3000/clintonolayiwola', {
@@ -11,19 +11,19 @@ app.use(express.static('public'))
 
 function addWeblink(name, url) {
   const addLink = sql`
-       INSERT INTO clint.link (name, url) VALUES (${name}, ${url})
+       INSERT INTO public.links (name, user_id, url) VALUES (${name},1, ${url})
    `
   addLink.execute()
 }
 app.get('/addWeblink', function (req, res) {
   console.log(req.query)
   addWeblink(req.query.name, req.query.url)
-  res.send('add')
+  //   res.send('add')
 })
 
 function deleteWeblink(id) {
   const deleteLink = sql`
-       DELETE FROM clint.link WHERE id = ${id}
+       DELETE FROM links WHERE id = ${id}
    `
   deleteLink.execute()
 }
@@ -35,7 +35,7 @@ app.get('/deleteWeblink', function (req, res) {
 
 async function getWeblinks() {
   const getLinks = sql`
-       SELECT * FROM clint.link
+       SELECT * FROM links
    `
   return getLinks
 }
@@ -43,9 +43,9 @@ app.get('/getWeblinks', async function (req, res) {
   res.send(await getWeblinks())
 })
 
-var server = app.listen(5000, function () {
-  var host = server.address().address
-  var port = server.address().port
+let server = app.listen(5000, function () {
+  let host = server.address().address
+  let port = server.address().port
 
   console.log('Example app listening at http://%s:%s', host, port)
 })
